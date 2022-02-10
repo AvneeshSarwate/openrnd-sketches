08f43d31ac981fb0e104f8f820eaa706bb570ae2
Avneesh Sarwate <avneeshsarwate@gmail.com>
2022-02-10 11:15:15 -0500

    Changes to allow building for M1 mac, v1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 enableFeaturePreview("VERSION_CATALOGS")
 rootProject.name = "openrndr-template"
 
-val openrndrUseSnapshot = false
-val orxUseSnapshot = false
+val openrndrUseSnapshot = true
+val orxUseSnapshot = true
 val ormlUseSnapshot = true
 
 dependencyResolutionManagement {
@@ -48,5 +48,6 @@ pluginManagement {
     repositories {
         mavenLocal()
         gradlePluginPortal()
+        maven(url="https://oss.sonatype.org/content/repositories/snapshots/")
     }
 }
